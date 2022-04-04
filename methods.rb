5a475336c81af3dd8a6322e0d5388c158fb500b0
Kassie Becker <kassaundralee@hotmail.com>
2022-04-04 17:23:05 -0500

    commit tree 8644993b8928eb72da4b1b8bb782a826cd6ede1b

--- methods.rb
+++ methods.rb
@@ -1,1 +1,24 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet name
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+def add(param1, param2)
+    param1 + param2
+end
+
+def halve(number)
+    if number.class == "number"
+        (number / 2)
+    else
+        nil
+    end
+end
